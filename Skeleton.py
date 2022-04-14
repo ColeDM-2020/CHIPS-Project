1643e6f64290520614607d358c372acc59fffbf6
Sarah Iaione <siaione@terpmail.umd.edu>
2022-04-14 17:16:14 -0400

    added def rolldice, def get_move, class chips and gamestate

--- Skeleton.py
+++ Skeleton.py
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
 #Setting Board as a Global Variable
-=======
->>>>>>> 0701312d98f34ce0fb2eb07bd11e1d2d5d336e9a
 from argparse import ArgumentParser
 from blessed import Terminal
 import sys
 
-<<<<<<< HEAD
 def rolldice(dice1, dice2)
 """"rolls two dice"""
 
@@ -16,14 +12,12 @@ def get_move(game, player):
 class Chips:
     """check if the dice value matches the values of get_move"""
     def__init__():
-=======
 #####
 # Board Setup
 #####
 
 class Player:
     def __init__(self):
->>>>>>> 0701312d98f34ce0fb2eb07bd11e1d2d5d336e9a
         pass
     
     def valid_move():
