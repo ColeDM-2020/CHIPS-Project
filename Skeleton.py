29c7b52c8ff3c8260bad8c623ac599b669d61e42
Cole Morgan <coledm20@gmail.com>
2022-04-14 17:22:58 -0400

    board

--- Skeleton.py
+++ Skeleton.py
@@ -3,15 +3,6 @@ from argparse import ArgumentParser
 from blessed import Terminal
 import sys
 
-def rolldice(dice1, dice2)
-""""rolls two dice"""
-
-def get_move(game, player):
-    """asks the player what chips they want to choose"""
-    
-class Chips:
-    """check if the dice value matches the values of get_move"""
-    def__init__():
 #####
 # Board Setup
 #####
@@ -20,6 +11,22 @@ TERM = Terminal()
 
 P1COLOR = TERM.green3
 P2COLOR = TERM.red3
+
+
+def rolldice(dice1, dice2): 
+    """"
+    rolls two dice
+    """
+    pass
+
+def get_move(game, player):
+    """asks the player what chips they want to choose"""
+    pass
+
+class Chips:
+    """check if the dice value matches the values of get_move"""
+    def__init__():
+        pass
 
 class Player:
     def __init__(self):
