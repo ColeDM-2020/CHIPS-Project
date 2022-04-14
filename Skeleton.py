bff3f47c9ce0d94609c9aca984bfe106c50b4856
Sarah Iaione <siaione@terpmail.umd.edu>
2022-04-14 17:34:11 -0400

    added stuff

--- Skeleton.py
+++ Skeleton.py
@@ -3,28 +3,24 @@ from argparse import ArgumentParser
 from blessed import Terminal
 import sys
 
-def rolldice(dice1, dice2):
-    """Rolls two dice
-    
-    Args:
-        dice1(int): The number you rolled on dice 1 that is 1-6.
-        dice2(int): The number you rolled on dice 2 that is 1-6.
-        
-    Side effect:
-        Prints out the number rolled on dice 1 and dice 2 to the terminal. 
-    
-    """
-    pass
+def rolldice(dice1, dice2)
+""""rolls two dice"""
+
 def get_move(game, player):
     """asks the player what chips they want to choose"""
     
 class Chips:
-    """check if the dice value matches the values of get_move
-    
-    Attribute:
-        player (str): player"""
-    def __init__():
-        pass
+    """check if the dice value matches the values of get_move"""
+    def__init__():
+        
+    def valid_move():
+        """This method checks whether a player is allowed to play from a particular pit.
+"""    
+    def play_round()
+    """This method manages one round of game play. It initializes the 
+      board for the new round, prints the board, manages turns until one player win"""   
+     
+    def game()
 #####
 # Board Setup
 #####
@@ -33,21 +29,11 @@ TERM = Terminal()
 
 P1COLOR = TERM.green3
 P2COLOR = TERM.red3
+
+
     
 class GameState:
     
 
-
-
-#Possible if name == main statement    
-if __name__ == "__main__":
-    args = parse_args(sys.argv[1:])
-    main(args., args., args., args.)
-    
-## Or this 
-if __name__ == "__main__":
-    args = parse_args(sys.argv[1:])
-    game = (args.name0, args.name1)
-    game.dbos()
 
 
