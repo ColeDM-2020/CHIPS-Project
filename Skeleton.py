23daec2b427eafdc71eb6e902e830b0262abb1ad
Emma McGraw <emcgraw@terpmail.umd.edu>
2022-04-14 17:06:11 -0400

    Possible if name = main statement

--- Skeleton.py
+++ Skeleton.py
@@ -3,3 +3,19 @@ class Player:
         pass
     def turn():
         pass
+
+
+
+#Possible if name == main statement    
+if __name__ == "__main__":
+    args = parse_args(sys.argv[1:])
+    main(args., args., args., args.)
+    
+## Or this 
+if __name__ == "__main__":
+    args = parse_args(sys.argv[1:])
+    game = (args.name0, args.name1)
+    game.player()
+
+    
+    
