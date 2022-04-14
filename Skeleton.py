5554d5a777a94ded299afc7dbd7f353d52c46f5a
Brooke Morgan <morganbrooke603@gmail.com>
2022-04-14 17:28:05 -0400

    changed a few things

--- Skeleton.py
+++ Skeleton.py
@@ -24,17 +24,6 @@ TERM = Terminal()
 
 P1COLOR = TERM.green3
 P2COLOR = TERM.red3
-
-class Player:
-    def __init__(self):
-        pass
-    def turn():
-        pass
-
-
-    
-    def valid_move():
-        pass
     
 class GameState:
     
