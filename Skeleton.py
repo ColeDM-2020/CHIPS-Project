648c02c5371e01cb520e569176a6d268ccf75292
Brooke Morgan <morganbrooke603@gmail.com>
2022-05-02 14:58:19 -0400

    trying to make gamestate and def state in class chips work

--- Skeleton.py
+++ Skeleton.py
@@ -32,29 +32,34 @@ class Dice:
         return result
 
 
-def get_move(game, player):
+def get_move1(game, player):
     """asks the player what chips they want to choose"""
     
     while True:
         print()
-        selection1 = (input(f"{game.names[player]}, select 1 chip that you would" 
+        selection = (input(f"{game.names[player]}, select 1 chip that you would" 
                             "like to choose (or enter q to quit): ")
                     .lower()
                     .strip())
         
-        selection2 = (input(f"{game.names[player]}, select 1 chip that you would" 
-                        "like to choose or enter '0' to skip this chip." 
-                        "(or enter q to quit): ")
+        if selection == "q":
+            sys.exit(0)
+        if type(selection) == int :
+            return selection
+def get_move2(game, player):
+    while True:
+        print()
+        selection = (input(f"{game.names[player]}, select 1 chip that you would" 
+                            "like to choose (or enter q to quit): ")
                     .lower()
                     .strip())
-        if selection1 == "q" or selection2 == "q":
+        if selection == "q":
             sys.exit(0)
-        if type(selection1) == int and type(selection2) == int:
-            value = selection1 + selection2
-            return value
+        if type(selection) == int :
+            return selection
 
 class GameState:
-    def __init__(self):
+    def __init__(self, selection1, selection2):
         """set attributes"""
         def num_or_dot(num, mask):
             if num in board:
@@ -64,17 +69,21 @@ class GameState:
         self.expr = ("^"
                      + "".join(num_or_dot(c, ".", re.escape) for c in word)
                      + "$")
+        self.selection1 = selection1
+        self.selection2 = selection2
     def __str__(self):
-        result = [self.board]
+        result = [self.board
+                  f"{self.selection1} and {self.selection2}, both chips have been removed from the board."]
         return result
     def match(self, s):
         return bool(re.search(self.expr, s.strip()))
 
 class Chips:
     """check if the dice value matches the values of get_move"""
-    def __init__(self, player, func = get_move):
+    def __init__(self, player, func0 = get_move1, func1 = get_move2):
         self.names = player
-        self.turn_funcs = func
+        self.func0 = func0
+        self.func1 = func1
         self.board =[]
         
     def valid_move(self, value):
@@ -86,12 +95,10 @@ class Chips:
         
         if value != roll.addroll():
             raise ValueError("Please pick chip(s) that add up to the sum of your roll")
-        
+    def state(self):
+        return GameState(self.func0, self.func1)   
     def play_round(self):
-        """This method manages one round of game play. It initializes the 
-      board for the new round, prints the board, manages turns until one player win"""   
-        pass
-    def game():
+        """This method manages one round of game play. it takes rolls the dice, takes the input, removes the chips"""   
         pass
     
     def game_over():
@@ -103,16 +110,6 @@ class Chips:
     def play():
         """Manage game play
         Ask if they want to play again and call play_again()"""
-    def play_again():
-        """Ask if they want to play another round"""
-        pass
-    def print_board(self, pause=PAUSE):
-        """Prints the board"""
-        template = (TEMPLATE
-                .replace("<NAME0>", self.names[0])
-                .replace("<NAME1>", self.names[1])
-                .replace("<SP>", " "*len(self.names[1])))
-        sleep(pause)
         
     def print_winner():
         "Prints the winnner"
