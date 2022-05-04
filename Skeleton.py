ec764f7646a606f581a9c103d3b4be866e5efda0
Brooke Morgan <morganbrooke603@gmail.com>
2022-05-04 18:36:48 -0400

    score game over

--- Skeleton.py
+++ Skeleton.py
@@ -1,12 +1,10 @@
 #Setting Board as a Global Variable
 from argparse import ArgumentParser
-from signal import pause
 from blessed import Terminal
-from time import sleep
 import sys
 import random
 import re
-
+from time import sleep
 
 ##Board Setup
 
@@ -22,7 +20,7 @@ TEMPLATE = """f{TERM.home+TERM.clear}\
 PAUSE = 0.2
 
 NUM0 = "abcdefghij"
-NUM1 = [9]
+NUM1 = 9
 
 class Dice:
     
@@ -76,7 +74,7 @@ def get_move(game, player):
         
         list_selections = list((int,selection().split()))
         
-        if selection == "quit":
+        if selection == "q":
             sys.exit(0)
         for x in list_selections:
             if type(x) == int:
@@ -158,7 +156,7 @@ class Chips:
     
         
         self.board = [1,2,3,4,5,6,7,8,9,10]
-        self.current_board() #need to create a current board method that shows the current board
+        self.current_board #need to create a current board method that shows the current board
         
         while self.game_over() == False:
                  
@@ -168,7 +166,7 @@ class Chips:
                         
             #need to update score here       
         
-    """def __str__(self):
+    def __str__(self):
         result = [self.board, 
                   f"{self.selection1} and {self.selection2}, both chips have been removed from the board."]
         for name, score in self.score.items():
@@ -178,24 +176,24 @@ class Chips:
         return "\n".join(result)
     
     def match(self, s):
-        return bool(re.search(self.expr, s.strip()))
+        return bool(re.search(self.expr, s.strip()))   
         
-    print(self.state().board)
+        """print(self.state().board)
         outcome = self.outcome()
         if outcome == "win":
             print(f"Your score for the round was {self.score()}")
         else:
             print(fThe game is not over.)"""
             
-    def current_board(self, pause=PAUSE):
-        """Displays the board in the terminal and pauses momentarily.
+    def print_board(self, pause=PAUSE):
+         """Displays the board in the terminal and pauses momentarily.
          
          """
         template = (TEMPLATE
                     .replace("<NAME0>", self.names[0])
                     .replace("<SP>", " "*len(self.names[1])))
         print(template.format(*(self.board[6::-1]+self.board[7:])))
-        sleep()
+        sleep(pause)
     
 def parse_args(arg):
     parser = ArgumentParser()
