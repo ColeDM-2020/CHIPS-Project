941085f4402094d1d33828d5d9ff72f7319c9593
Cole Morgan <coledm20@gmail.com>
2022-05-04 20:12:02 -0400

    change

--- CHIPS.py
+++ CHIPS.py
@@ -6,3 +6,19 @@ import random
 import re
 from time import sleep
 
+TERM = Terminal()
+
+PCOLOR = TERM.red2       
+NCOLOR = TERM.cyan2
+PNAME = TERM.green3
+
+SLOT = "{:>0}"
+TEMPLATE = f"""{TERM.home+TERM.clear}\
+<SP>{PCOLOR}<NAME>  {SLOT}  {SLOT}  {SLOT}  {SLOT}  {SLOT}  {SLOT}  {SLOT}  {SLOT}  {SLOT}  {SLOT}
+<SP>{NCOLOR}------------------------------------
+{PNAME}       a  b  c  d  e  f  g  h  i  j {TERM.normal}"""
+
+PAUSE = 0.2
+
+NUM0 = "abcdefghij"
+NUM1 = [9]
